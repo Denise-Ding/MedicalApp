d795d0af6f90c3c723c348e8b7d0bf4cd014887b
Denise Ding <denise.ding@horizonpower.com.au>
2025-07-26 14:22:39 +0800

    Initial commit

---
+++ MedWebApiApp/Models/Provider.cs
@@ -0,0 +1,11 @@
+namespace MedWebApiApp.Models
+{
+    public class Provider
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Hospital { get; set; }
+        public bool Doctor { get; set; }
+
+    }
+}
---
+++ MedWebApiApp/Services/DataSource.cs
@@ -0,0 +1,25 @@
+﻿namespace MedWebApiApp.Services
+{
+    public class DataSource
+    {
+        private string defaultDataFilesFolder = "DataFolder";
+        private string defaultProviderFileName = "defaultProvider.txt";
+
+        public DataSource()
+        {
+            Console.WriteLine("DataSource constructor called");
+
+            try
+            {
+                //LoadData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading data: {ex.Message}");
+
+            }
+
+        }
+
+    }
+}
